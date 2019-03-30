6dd0359d74daca83a350bfc1277fbf5a76df37ad
WILLIXfr <william.alloul@gmail.com>
2019-03-30 03:17:13 +0100

    Mutli tracker handling and update usage

--- README.md
+++ README.md
@@ -1,13 +1,38 @@
-# georide_api_usage
+# Georide_api_usage
 
-Be sure to initialize the 2 variables email and password in usage.py
-Please make usage.py executable before any action
+georide_api_usage is a repo used to store multiple ways of usage of the api.georide.fr
 
-There is 2 cases of usage:
+With the current script you can:
+  - Lock/Unlock your first tracker
+  - Lock/Unlock by tracker ID
+  - Lock/Unlock by tracker Name
+  - List all of you trackers
 
-Locking your tracker
-python path/to/usage.py lock
+## Setup
 
-Unlocking your tracker
-python path/to/usage.py unlock
+  - Refer your login and password in the two variables and let's go !
 
+
+### Usage
+
+georide_api_usage requires python3 to run.
+
+To lock your first tracker
+```sh
+$ python usage.py --action lock
+```
+
+To unlock the XXX tracker
+```sh
+$ python usage.py --action unlock --id XXX
+```
+
+To lock the SSS tracker
+```sh
+$ python usage.py --action lock --name SSS
+```
+
+To list all of your trackers
+```sh
+$ python usage.py --action list
+```
--- usage.py
+++ usage.py
@@ -4,6 +4,10 @@
 import requests
 import json
 import sys
+import argparse
+
+g_tracker_id=None
+g_tracker_name=None
 
 def login():
   email=''
@@ -13,11 +17,29 @@ def login():
   authToken="Bearer " + log_request["authToken"]
   return authToken
 
-def tracker_info(token):
+def get_trackers(token):
+  auth_head = { 'Authorization': token}
+  result = requests.get("https://api.georide.fr/user/trackers", headers=auth_head).json()
+
+  return result
+
+def get_first_tracker_info(token):
   auth_head = { 'Authorization': token}
   result = requests.get("https://api.georide.fr/user/trackers", headers=auth_head).json()
 
   return result[0]["trackerId"],result[0]["trackerName"]
+
+def check_tracker_exist(token, trackerId, trackerName):
+  auth_head = { 'Authorization': token}
+  result = requests.get("https://api.georide.fr/user/trackers", headers=auth_head).json()
+  for tracker in result:
+    if (str(tracker['trackerId']) == trackerId or str(tracker['trackerName']) == trackerName):
+      global g_tracker_id
+      g_tracker_id=tracker['trackerId']
+      global g_tracker_name
+      g_tracker_name=tracker['trackerName']
+      return True
+  return False
 
 def unlock(token, trackerId):
   auth_head = { 'Authorization': token}
@@ -31,17 +53,49 @@ def lock(token, trackerId):
   url = "https://api.georide.fr/tracker/" + str(trackerId) + "/lock"
   requests.post(url, headers=auth_head, data=data)
 
-if (sys.argv[1] and sys.argv[1] == 'unlock'):
-  token=login()
-  trackerId,trackerName=tracker_info(token)
-  unlock(token,trackerId)
-  print (trackerName + ' is unlocked')
-elif (sys.argv[1] and sys.argv[1] == 'lock'):
-  token=login()
-  trackerId,trackerName=tracker_info(token)
-  lock(token,trackerId)
-  print (trackerName + ' is locked')
-else:
-  print ("Wrong Argument")
+def display_trackers(trackers_list):
+  for tracker in trackers_list:
+    print ('Tracker Name:\t' + str(tracker['trackerName']) + '\nTracker Id:\t' + str(tracker['trackerId']) + '\n')
 
+def main():
+  parser = argparse.ArgumentParser(description='Action required')
+  parser.add_argument('--action', dest='action', choices=['lock', 'unlock', 'list'], help='action desired')
+  parser.add_argument('--id', dest='tracker_id', nargs='?', help='tracker_id')
+  parser.add_argument('--name', dest='tracker_name', nargs='*', help='tracker_name')
+  args = parser.parse_args()
+
+
+  if (args.action == 'unlock' and (args.tracker_id == None and args.tracker_name == None)):
+    print ('Unlocking 1st tracker')
+    token=login()
+    trackerId,trackerName=get_first_tracker_info(token)
+    unlock(token,trackerId)
+    print (trackerName + ' is unlocked')
+  elif (args.action == 'unlock' and (args.tracker_id != None or args.tracker_name != None)):
+    print ('Unlocking selected tracker')
+    token=login()
+    if (check_tracker_exist(token,args.tracker_id,args.tracker_name)):
+      unlock(token,g_tracker_id)
+      print (g_tracker_name + ' is unlocked')
+  elif (args.action == 'lock' and (args.tracker_id == None and args.tracker_name == None)):
+    print ('Locking 1st tracker')
+    token=login()
+    trackerId,trackerName=get_first_tracker_info(token)
+    lock(token,trackerId)
+    print (trackerName + ' is locked')
+  elif (args.action == 'lock' and (args.tracker_id != None or args.tracker_name != None)):
+    print ('Locking selected tracker')
+    token=login()
+    if (check_tracker_exist(token,args.tracker_id,args.tracker_name)):
+      lock(token,g_tracker_id)
+      print (g_tracker_name + ' is locked')
+  elif (args.action == 'list'):
+    token=login()
+    trackers_list=get_trackers(token)
+    display_trackers(trackers_list)
+  else:
+    print ("Wrong Argument")
+
+if __name__== "__main__":
+  main()
 
